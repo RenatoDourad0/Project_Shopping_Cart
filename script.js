58c65d4e3f6a347edd08221ff4ef86f6a6944ccd
Renato Dourad0 <99191410+RenatoDourad0@users.noreply.github.com>
2022-07-12 14:02:07 -0300

    requisitos 10 e 11

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const itens = document.querySelector('.items');
+const cartItems = document.querySelector('.cart__items');
+const emptyCart = document.querySelector('.empty-cart');
+const bodyelement = document.querySelector('body');
+const cartProductsArray = [];
+
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -28,6 +34,11 @@ const cartItemClickListener = (event) => {
   const placeholder = event.target.parentElement;
   placeholder.removeChild(event.target);
   localStorage.removeItem('cartItems');
+  // const product = event.target.innerText;
+  // const price = parseInt(product.substring(product.length - 10, product.length - 1), 10);
+  // console.log(price)
+  // const currentPrice = parseInt(document.querySelector('.total-price').innerText, 10);
+  // document.querySelector('.total-price').innerText = `${currentPrice - price}`;
 };
 
 const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
@@ -38,11 +49,7 @@ const createCartItemElement = ({ id: sku, title: name, price: salePrice }) => {
   return li;
 };
 
-const itens = document.querySelector('.items');
-const cartItems = document.querySelector('.cart__items');
-const cartProductsArray = [];
-
-const LocalStorageSavedCartItems = () => {
+const getLocalStorageSavedCartItems = () => {
   if (localStorage.length > 0) {
     const savedStringItems = getSavedCartItems();
     const savedItems = savedStringItems.substring(0, savedStringItems.length - 3).split(' / ,');
@@ -56,20 +63,60 @@ const LocalStorageSavedCartItems = () => {
   }
 };
 
-window.onload = async () => {
-  LocalStorageSavedCartItems();
+// const createTotalPriceElement = () => {
+//   const newLi = document.createElement('li');
+//   newLi.className = 'total-price';
+//   newLi.innerText = '0';
+//   cartItems.appendChild(newLi);
+// };
 
-  const productsArray = await fetchProducts('computador');
-  productsArray.results.forEach((element) => {
-    const section = createProductItemElement(element);
-    itens.appendChild(section);
+const emptyCartOnClick = () => {
+  emptyCart.addEventListener('click', () => {
+    const childrenElements = Array.from(cartItems.children);
+    childrenElements.forEach((element) => cartItems.removeChild(element));
+    localStorage.removeItem('cartItems');
   });
+};
 
+const createLoadingElement = () => {
+  const newDiv = document.createElement('h2');
+  newDiv.className = 'loading';
+  newDiv.innerText = 'carregando...';
+  newDiv.style.backgroundColor = 'white';
+  newDiv.style.position = 'absolute';
+  newDiv.style.padding = '20px';
+  newDiv.style.top = '50vh';
+  newDiv.style.left = '50vw';
+  return newDiv;
+};
+
+const getProductsArrayFunc = async () => {
+  const loading = bodyelement.appendChild(createLoadingElement());
+  const productsArray = await fetchProducts('computador');
+  bodyelement.removeChild(loading);
+  productsArray.results.forEach((element) => {
+  const section = createProductItemElement(element);
+  itens.appendChild(section);
+  });
+};
+
+const getCartProductsOnClick = () => {
   itens.addEventListener('click', async (event) => {
     const product = await fetchItem(event.target.parentElement.firstChild.innerText);
     const cartProduct = createCartItemElement(product);
     cartItems.appendChild(cartProduct);
     cartProductsArray.push(`${cartProduct.innerText} / `);
     saveCartItems(cartProductsArray);
+
+    // const productPrice = parseInt(product.price, 10);
+    // const currentPrice = parseInt(document.querySelector('.total-price').innerText, 10);
+    // document.querySelector('.total-price').innerText = `${productPrice + currentPrice}`;
   });
+};
+
+window.onload = () => {
+  getLocalStorageSavedCartItems();
+  getProductsArrayFunc();
+  emptyCartOnClick();
+  getCartProductsOnClick();
  };
